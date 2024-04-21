10aed5fae373ee36034187b2d80040b200bb5d15
sbergeroncp <93004917+sbergeroncp@users.noreply.github.com>
2024-04-21 19:52:58 -0400

    Mise à jour pxt.json, main.blocks, jeu_6a.md, jeu_6.md, tutoriel_6.md, images.g.jres, images.g.ts, vaisseau_1.md

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "vaisseau":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . 9 1 1 9 . . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . 1 1 9 9 1 1 . . . . . 
+. . . 1 1 1 1 9 9 1 1 1 1 . . . 
+. . . 1 1 1 1 1 1 1 1 1 1 . . . 
+. . . 1 . 1 1 1 1 1 1 . 1 . . . 
+. . . 1 . . 1 9 9 1 . . 1 . . . 
+. . . 1 1 . 1 9 9 1 . . 1 . . . 
+. . . 1 . . 1 1 1 1 . 1 1 . . . 
+. . 1 1 . . . 4 2 . . . 1 1 . . 
+. . . . . . 5 2 4 5 . . . . . . 
+. . . . . 2 . 4 2 . 2 . . . . . 
+. . . . 2 5 . 2 2 . 5 2 . . . . 
+`;
+            case "image2":
+            case "vaisseau0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . 9 1 1 9 . . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . 1 1 9 9 1 1 . . . . . 
+. . . 1 1 1 1 9 9 1 1 1 1 . . . 
+. . . 1 1 1 1 1 1 1 1 1 1 . . . 
+. . . 1 . 1 1 1 1 1 1 . 1 . . . 
+. . . 1 . . 1 9 9 1 . . 1 . . . 
+. . . 1 1 . 1 9 9 1 . . 1 . . . 
+. . . 1 . . 1 1 1 1 . 1 1 . . . 
+. . 1 1 . . . 4 2 . . . 1 1 . . 
+. . . . . . 5 2 4 5 . . . . . . 
+. . . . . 2 . 4 2 . 2 . . . . . 
+. . . . 2 5 . 2 2 . 5 2 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
---
+++ vaisseau_1.md
@@ -0,0 +1,1 @@
+# vaisseau_1
